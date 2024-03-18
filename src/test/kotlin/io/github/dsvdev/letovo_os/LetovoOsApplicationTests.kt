6b0e3fbb1c618f6d32cad22e208b9f6e3532a61a
dm.sviridov <dm@svrdv.ru>
2024-03-18 12:01:32 +0300

    removed test

--- src/test/kotlin/io/github/dsvdev/letovo_os/LetovoOsApplicationTests.kt
+++ src/test/kotlin/io/github/dsvdev/letovo_os/LetovoOsApplicationTests.kt
@@ -1,13 +1,8 @@
 package io.github.dsvdev.letovo_os
 
-import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 class LetovoOsApplicationTests {
-
-	@Test
-	fun contextLoads() {
-	}
 
 }
